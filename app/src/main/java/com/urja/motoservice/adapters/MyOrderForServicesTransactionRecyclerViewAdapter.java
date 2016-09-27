4962a91d22e64d3107a35d16b33b9fea17f86015
bapisth <hemendra7011@rediffmail.com>
2016-09-27 23:00:50 +0530

    These files were missing in github.

---
+++ app/src/main/java/com/urja/motoservice/adapters/MyOrderForServicesTransactionRecyclerViewAdapter.java
@@ -0,0 +1,131 @@
+package com.urja.motoservice.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.urja.motoservice.R;
+import com.urja.motoservice.fragment.TransactionDetailActivityFragment.OnListFragmentInteractionListener;
+import com.urja.motoservice.fragment.dummy.DummyContent.DummyItem;
+import com.urja.motoservice.model.Transaction;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyOrderForServicesTransactionRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final String TAG = MyOrderForServicesTransactionRecyclerViewAdapter.class.getSimpleName();
+    private final List<Transaction> mValues;
+    private final OnListFragmentInteractionListener mListener;
+    private static final int VIEW_TYPE_EMPTY_LIST_PLACEHOLDER = 0;
+    private static final int VIEW_TYPE_OBJECT_VIEW = 1;
+
+    public MyOrderForServicesTransactionRecyclerViewAdapter(List<Transaction> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TransactionViewHolder transactionViewHolder = null;
+
+        RecyclerView.ViewHolder viewHolder = null;
+        View view = null;
+        switch (viewType){
+            case VIEW_TYPE_EMPTY_LIST_PLACEHOLDER:
+                view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_item, parent, false);
+                viewHolder = new EmptyTransactionViewHolder(view);
+                break;
+            case VIEW_TYPE_OBJECT_VIEW:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_orderforservicestransaction, parent, false);// fragment_item
+                viewHolder = new TransactionViewHolder(view);
+                break;
+        }
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()){
+            case VIEW_TYPE_OBJECT_VIEW:
+                TransactionViewHolder transactionViewHolder = (TransactionViewHolder) holder;
+                Transaction transaction = mValues.get(position);
+                transactionViewHolder.mItem = transaction;
+                //holder.mIdView.setText(mValues.get(position).id);
+                //holder.mContentView.setText(mValues.get(position).content);
+                String carNumber = transaction.getServiceRequestList().get(0).getCarnumber();
+                transactionViewHolder.mCarNumber.setText(carNumber);
+                transactionViewHolder.mTransactionID.setText(transaction.getTransactionId());
+
+                transactionViewHolder.mTransactionDate.setText(transaction.getServiceRequestDate());
+                transactionViewHolder.mTransactionStatus.setText(transaction.getRequestStatus().toUpperCase());
+                break;
+            case VIEW_TYPE_EMPTY_LIST_PLACEHOLDER:
+                Log.e(TAG, "onBindViewHolder: EMPTY TEXT VIEW" );
+                EmptyTransactionViewHolder emptyTransactionViewHolder = (EmptyTransactionViewHolder) holder;
+                emptyTransactionViewHolder.mEmptyText.setText("No Data Available!");
+                break;
+        }
+
+
+        /*holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mValues.isEmpty()) {
+            return VIEW_TYPE_EMPTY_LIST_PLACEHOLDER;
+        } else {
+            return VIEW_TYPE_OBJECT_VIEW;
+        }
+    }
+
+    public class TransactionViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mCarNumber;
+        public final TextView mTransactionID;
+        public final TextView mTransactionDate;
+        public final TextView mTransactionStatus;
+        public Transaction mItem;
+
+        public TransactionViewHolder(View view) {
+            super(view);
+            mView = view;
+            mCarNumber = (TextView) view.findViewById(R.id.carNumber);
+            mTransactionID = (TextView) view.findViewById(R.id.transactionID);
+            mTransactionDate = (TextView) view.findViewById(R.id.transactionDate);
+            mTransactionStatus = (TextView) view.findViewById(R.id.transactionStatus);
+        }
+    }
+
+    public class EmptyTransactionViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mEmptyText;
+        public EmptyTransactionViewHolder(View itemView) {
+            super(itemView);
+            mEmptyText = (TextView) itemView.findViewById(R.id.no_data);
+        }
+    }
+}
